10c21c1855df5a78f1335fac4775e3b4fa0bb1e8
ganderson <gandahkelvin33@gmail.com>
2025-03-24 10:29:09 +0000

    first commit

---
+++ src/electron/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "path";
+import { isDev, ipcMainHandle } from "./utils.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { getPreloadPath, getUIPath } from "./pathResolver.js";
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    }
+  });
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadURL(getUIPath());
+  }
+
+  pollResources(mainWindow);
+
+  ipcMainHandle('getStaticData', () => getStaticData());
+});
